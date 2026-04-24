7f15fd38a2ac2e87074f564de051a99bf381e165
GmesVictor <smithvictor476@gmail.com>
2026-04-24 19:24:08 -0300

    Atualização

---
+++ JS-comecando-sua-jornada/Conhecendo_switch/app.js
@@ -0,0 +1,18 @@
+// Aprendendo Loops
+
+// 1° while = enquanto a condição for verdadeira, o código dentro do while será executado.
+
+var x = 5;
+
+while(x < 10){
+    document.write("<h3 style='color: red;'> <br> o valor do X é: " + x + " </h3>");
+
+    // aumentar o valor de X. ex: X = X + 1
+    x++;
+}
+
+// FOR = PARA 
+
+for(a = 0; a <= 10; a++){
+    document.write("<h3 style='color: blue;'> <br> o valor do A é: " + a + " </h3>");
+}
